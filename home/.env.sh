95f23aeccc4505d0449a20c2c92598e3d281dbfd
James Reuss <jamesreuss@gmail.com>
2020-06-09 21:13:01 +0100

    added GOROOT to env.sh

--- home/.env.sh
+++ home/.env.sh
@@ -1,7 +1,8 @@
 # common static env here
 export HOME=~
+export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$PATH
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 export GOPRIVATE=github.com/jimjibone/*
 # export EDITOR=vim-wrapper
 export PAGER="less -R"
