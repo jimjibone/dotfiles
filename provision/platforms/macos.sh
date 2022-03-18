dd638e2babb3765bc6fe6f55244d31605e3766b1
James Reuss <jamesreuss@gmail.com>
2022-03-18 13:40:20 +0000

    updated macos provision script

--- provision/platforms/macos.sh
+++ provision/platforms/macos.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # macos -- get homebrew
-if [ ! -f /usr/local/bin/brew ]; then
+if [ which figlet &>/dev/null ]; then
     echo "Installing homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "Updating homebrew"
     brew update
 fi
 
 # upgrade or install gui apps (logic necessary)
-echo "Installing/upgrading brew casks"
-packages=(visual-studio-code 1password contexts iterm2 firefox vlc)
-for package in "${packages[@]}"; do
-    brew cask upgrade $package || brew cask install $package
-done
+# echo "Installing/upgrading brew casks"
+# packages=(visual-studio-code 1password contexts iterm2 firefox vlc)
+# for package in "${packages[@]}"; do
+#     brew cask upgrade $package || brew cask install $package
+# done
 
 # upgrade or install cli things (logic necessary)
 echo "Installing/upgrading brew packages"
-packages=(mosh figlet keychain golang protobuf nodejs ffmpeg schollz/tap/croc tmux)
+packages=(mosh figlet keychain golang protobuf nodejs tmux)
 for package in "${packages[@]}"; do
     brew upgrade $package || brew install $package
 done
