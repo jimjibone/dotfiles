08ae7eb81b1371730a5d64c5f034286b5c1943d3
James Reuss <james.reuss@horiba-mira.com>
2022-07-28 09:07:39 +0100

    fixed debian provision script

--- provision/platforms/debian.sh
+++ provision/platforms/debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get -y update
 sudo apt-get -y install git vim zsh mosh tmux figlet keychain
-sudo cp config/sshd_config /etc/ssh/sshd_config
+sudo cp provision/config/sshd_config /etc/ssh/sshd_config
