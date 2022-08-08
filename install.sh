56279728810082897e34ddb1fc276bc1ed9a38db
James Reuss <james.reuss@horiba-mira.com>
2022-08-08 13:10:15 +0100

    added vim plugin install step

--- install.sh
+++ install.sh
@@ -23,6 +23,10 @@ test -f ~/.ssh/id_rsa.pub && chmod 644 ~/.ssh/id_rsa.pub
 cp -r home/.??* ~ 2> /dev/null
 # cp -a bin ~
 
+# install vim plugins
+echo -e "${GREEN}Installing vim plugins${NC}"
+vim -c 'PlugInstall' -c 'qa'
+
 # init conda if installed
 if which conda &>/dev/null; then
 	conda init $(basename $SHELL) &>/dev/null
