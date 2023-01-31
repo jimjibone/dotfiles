6cb89906c89481fa824760bb4dff014e05afdeab
jamesreuss <james.reuss@mira-ugv.com>
2023-01-31 10:18:11 +0000

    removed keychain and sshd_config from debian provision

--- provision/platforms/debian.sh
+++ provision/platforms/debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y install git vim zsh mosh tmux figlet keychain
-sudo cp provision/config/sshd_config /etc/ssh/sshd_config
+sudo apt -y update
+sudo apt -y install git vim zsh mosh tmux figlet
+#sudo cp provision/config/sshd_config /etc/ssh/sshd_config
