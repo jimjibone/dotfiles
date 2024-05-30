7e93572bf6dd946c0f8554ee73f1b490b7505daf
jimjibone <jamesreuss@gmail.com>
2024-05-30 17:14:29 +0100

    p10k show exit code of last command

--- v2/.p10k.zsh
+++ v2/.p10k.zsh
@@ -52,6 +52,7 @@
     context                   # user@host
     dir                       # current directory
     vcs                       # git status
+    status                    # exit code of the last command
     command_execution_time    # previous command duration
     # =========================[ Line #2 ]=========================
     newline                   # \n
