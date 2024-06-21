fc1ce55b12851edaeb48ac40b86f86984ea4104b
jimjibone <jamesreuss@gmail.com>
2024-06-21 22:54:32 +0100

    errm whitespace

--- v2/install.sh
+++ v2/install.sh
@@ -15,8 +15,8 @@ NC='\033[0m' # No Color
 # setup ssh dir and files
 echo -e "${GREEN}Setting up .ssh directory and files${NC}"
 if [ ! -d ~/.ssh ]; then
-    mkdir ~/.ssh
-    chmod 700 ~/.ssh
+	mkdir ~/.ssh
+	chmod 700 ~/.ssh
 fi
 touch ~/.ssh/known_hosts
 touch ~/.ssh/authorized_keys
@@ -29,8 +29,8 @@ test -f ~/.ssh/id_rsa.pub && chmod 644 ~/.ssh/id_rsa.pub
 # copy dotfiles separately, normal glob does not match
 echo -e "${GREEN}Copying dotfiles${NC}"
 #cp -r home/.??* ~ 2> /dev/null
-cp .zshrc ~ 2> /dev/null
-cp .p10k.zsh ~ 2> /dev/null
+cp .zshrc ~ 2>/dev/null
+cp .p10k.zsh ~ 2>/dev/null
 # cp -a bin ~
 
 # install vim plugins - archived, now using nvim
@@ -42,14 +42,14 @@ set -e
 
 # display the splash
 if which figlet &>/dev/null; then
-    figlet -f slant dotfiles
+	figlet -f slant dotfiles
 else
-    echo -e ${GREY}"figlet missing${NC}"
+	echo -e ${GREY}"figlet missing${NC}"
 fi
 echo -e "${GREEN}Install complete!${NC}"
 
 # print recommendations
 # echo "Add ssh key to your keychain with: ssh-add -K ~/.ssh/id_rsa"
 if [[ $SHELL != *zsh* ]]; then
-    echo -e "${RED}Switch to zsh!${NC} chsh -s \$(which zsh)"
+	echo -e "${RED}Switch to zsh!${NC} chsh -s \$(which zsh)"
 fi
