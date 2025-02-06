ef9e69043750daf8b7a26680619bc9a465bcb102
jimjibone <jamesreuss@gmail.com>
2025-02-06 23:15:39 +0000

    add eza source to apt

--- provision.sh
+++ provision.sh
@@ -112,6 +112,12 @@ elif grep -q Debian /etc/issue; then
 		PACKAGES="$PACKAGES zsh"
 	fi
 	if yes_or_no "install eza (ls replacement)?"; then
+		if ! apt list --installed 2>/dev/null | grep -q "^eza/"; then
+			sudo mkdir -p /etc/apt/keyrings
+			wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+			echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+			sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+		fi
 		PACKAGES="$PACKAGES eza"
 	fi
 	if yes_or_no "install fzf (fuzzy search)?"; then
